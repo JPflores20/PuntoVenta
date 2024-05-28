941e54128a82ffb724b7ed7490f78a36acd4bfbd
JPflores20 <99496617+JPflores20@users.noreply.github.com>
2024-05-28 14:55:06 -0600

    03. Creacion de modelos, views, htmls
    Creamos los modelos para la base de datos, las views correspondientes a dichos modelos y modificamos el archivo admin.py

--- PuntoVenta/ventas/urls.py
+++ PuntoVenta/ventas/urls.py
@@ -2,5 +2,7 @@ from django.urls import path, include
 from. import views
 urlpatterns = [
     path('', views.ventas_view, name= 'Ventas'),
+    path('clientes/', views.clientes_view, name= 'Clientes'),
+
 ]
 
